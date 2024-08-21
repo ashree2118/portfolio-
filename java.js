0cde3da93c49d97f580c132c255e02729ace3f47
Anushree <145786601+ashree2118@users.noreply.github.com>
2024-08-21 22:24:08 +0530

    Update java.js

--- java.js
+++ java.js
@@ -14,7 +14,7 @@ let resumeButton = document.querySelector(" button");
 resumeButton.addEventListener("click", resume);
 
 var typed = new Typed('#element', {
-    strings: ['Web Developer'],
+    strings: ['Web Developer.'],
     typeSpeed: 107,
     backspeed: 90,
     loop: true,
